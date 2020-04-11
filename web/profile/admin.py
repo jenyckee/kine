def99cb7c0b85e1f4a081a73b7233f178c7cca53
jeroen <jenyckee@MacBook-Pro-de-jeroen.local>
2020-04-11 15:36:16 +0200

    Add profiles to admin

--- web/profile/admin.py
+++ web/profile/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+
+from .models import User, Patient, Therapist
+
+
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'username', 'is_patient', 'is_therapist', 'password1', 'password2')
+        }),
+        ('Permissions', {
+            'fields': ('is_superuser', 'is_staff')
+        })
+    )
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'username', 'is_patient', 'is_therapist', 'password')
+        }),
+        ('Permissions', {
+            'fields': ('is_superuser', 'is_staff')
+        })
+    )
+    list_display = ['email', 'username', 'is_patient', 'is_therapist']
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+admin.site.register(Therapist)
+admin.site.register(Patient)
+admin.site.register(User, UserAdmin)
+# admin.site.unregister(Group)
