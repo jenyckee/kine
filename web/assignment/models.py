2189ebf55c032891fa8c0e7e23c2ee6cde402863
jeroen <jenyckee@MacBook-Pro-de-jeroen.local>
2020-04-22 16:17:10 +0200

    shmuck the formatting

--- web/assignment/models.py
+++ web/assignment/models.py
@@ -3,6 +3,7 @@ from profile.models import User
 from exercise.models import Exercise
 
 class Assignment(models.Model):
-  exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-  owner = models.ForeignKey(User, related_name='assignments', on_delete=models.CASCADE)
-  completed = models.BooleanField()
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, related_name='assignments', on_delete=models.CASCADE)
+    completed = models.BooleanField()
+    
