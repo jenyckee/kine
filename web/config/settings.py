94d6871a16dd6c39c49e6cac869bd5ef0a4e689a
jeroen <jenyckee@MacBook-Pro-de-jeroen.local>
2020-11-05 18:55:40 +0100

    add django filters

--- web/config/settings.py
+++ web/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_filters',
 
     'rest_framework',
     'rest_framework.authtoken',
